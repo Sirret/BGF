6eaee807b7ca2588aad44d6742fa859095e6ecf8
Sirret <103698757+Sirret@users.noreply.github.com>
2026-07-05 02:04:06 +0000

    testing correct pathing

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,18 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
-// https://astro.build/config
 export default defineConfig({
 	site: 'https://sirret.github.io',
 	base: '/BGF',
 	vite: {
 		plugins: [tailwindcss()],
+		resolve: {
+			alias: {
+				'@': '/src',
+				'@components': '/src/components',
+				'@layouts': '/src/layouts',
+				'@pages': '/src/pages',
+			},
+		},
 	},
 });
